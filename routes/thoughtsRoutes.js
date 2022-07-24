313800c2df4ae8f73cc585db6006aa23a6c68c5f
juliadiasp <juliamarinae@gmail.com>
2022-07-24 14:32:30 -0300

    Initial commit

---
+++ routes/thoughtsRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ToughtController = require("../controllers/ThoughtController");
+const router = express.Router();
+const ThoughtController = require("../controllers/ThoughtController");
+
+// helpers
+const checkAuth = require("../helpers/auth").checkAuth;
+
+router.get("/add", checkAuth, ThoughtController.createThought);
+router.post("/add", checkAuth, ThoughtController.createThoughtSave);
+router.get("/edit/:id", checkAuth, ThoughtController.updateThought);
+router.post("/edit", checkAuth, ThoughtController.updateThoughtSave);
+router.get("/dashboard", checkAuth, ThoughtController.dashboard);
+router.post("/remove", checkAuth, ThoughtController.removeThought);
+router.get("/", ThoughtController.showThoughts);
+
+module.exports = router;
